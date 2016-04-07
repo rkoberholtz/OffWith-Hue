1d01f8abcf723d5f773e66dcdbd5cdffe1934723
Rich <rich@rickelobe.com>
2016-04-07 18:31:25 -0400

    Adding outline of script

---
+++ hue_off.py
@@ -0,0 +1,21 @@
+import sys
+from qhue import bridge
+from qhue import create_new_username
+
+def main():
+
+	# IP address of your Hue Bridge
+	bridge = 
+	
+	# How often (in seconds) to check the pwrstatd log for events
+	check_interval = 60
+
+
+
+def createUsername():
+	# This function will create a new user account on the Hue bridge if the '-c' argument is passed
+
+
+
+if __name__ == "__main__":
+	main()
