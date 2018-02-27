ce0ee158a807e95cde99d7d1f902cc1879f4c16d
Rich Oberholtzer <rich@rickelobe.com>
2018-02-27 15:40:53 -0500

    Removed license blurb in comments

--- hue_off.py
+++ hue_off.py
@@ -15,21 +15,6 @@
 	rich@rickelobe.com
 	https://gitlab.rickelobe.com
 
-Copyright (c) 2016, Richard K. Oberholtzer
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 """
 
 import sys
